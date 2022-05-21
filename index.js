1ef29884043d02bc49c412a2d896b1b3015daf22
Natalia Barros <natalia.vieiramonteiro70@gmail.com>
2022-05-20 21:43:36 -0300

    fim da sessao 2.10

--- index.js
+++ index.js
@@ -34,4 +34,46 @@ const solicitarCashback=(nome,valor)=>{
             nome,valor
         }
     }
+}//arrow function 
+//hisotiricoDePedidosCashBackReducer
+//recebe uma fatia de estado sobre a qual operar (uma lista chamada historicoDePedidosCashback.Por padrão,ela é uma lista vazia)
+//recebe uma ação
+//devolve: uma lista que contém todos os pedidos da lista recebida e o pedido descrito no payload da ação
+//detalhe : somente operar no estado se o type for apropriado
+//detalhe2 :obrigatorio usar o spread(...)
+const historicoDePedidosCashbackReducer=(historicoDePedidosCashback=[],acao)=>{
+    if(acao.type==="SOLICITAR-CASHBACK"){
+    return[
+        ...historicoDePedidosCashback,acao.payload
+        ]
+    }
+    return HistoricoDePedidosCashback
+ }
+//implementar o reducer que manipula o caixa 
+// o caixa começa zerado
+
+function caixaReducer(valorEmCaixa=0,acao){
+if(acao.type==="SOLICITAR-CASHBACK" )
+{
+    return valorEmCaixa-acao.payload.valor
+}
+if (acao.type==="CRIAR_CONTRATO " )
+    return valorEmCaixa+acao.payload.valor
+return valorEmCaixa
+}
+
+//implementar o reducer que lida com a lista de contratos
+// ele pode criar contratos e cancelar contratos
+
+
+
+const contratosReducer=(listaContratosAtual=[],acao)=>{
+    if(acao.type==="CRIAR_CONTRATO"){
+        return[listaContratosAtual,acao.payload]
+    }
+    if(acao.type==="CANCELAR_CONTRATO")
+    {
+        return listaContratosAtual.filter(contrato=>contrato.nome!==acao.payload.nome)
+    }
+    return listaContratosAtual
 }
